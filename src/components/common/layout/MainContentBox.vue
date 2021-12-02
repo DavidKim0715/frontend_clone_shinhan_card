d064a9d5d55970f44841099c17605361d8f8a1ee
DavidKim0715 <sunkit0526@gmail.com>
2021-12-02 16:25:29 +0900

    [ADD] Added main box component

---
+++ src/components/common/layout/MainContentBox.vue
@@ -0,0 +1,11 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+const MainContentBox = defineComponent({
+  name: "MainContentBox",
+});
+export default MainContentBox;
+</script>
