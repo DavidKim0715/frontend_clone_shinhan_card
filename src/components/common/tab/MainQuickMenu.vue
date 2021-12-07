62e3e2b6eb10edd6f5c5e22f879df2ee257e41b4
DaivdKim0715 <sunkit0526@gmail.com>
2021-12-07 17:15:41 +0900

    [MOD] Modified quick menu css

--- src/components/common/tab/MainQuickMenu.vue
+++ src/components/common/tab/MainQuickMenu.vue
@@ -13,46 +13,46 @@ const MainQuickMenu = defineComponent({
 export default MainQuickMenu;
 </script>
 <template>
-  <nav class="main_quick_menu">
-    <ul class="menu_list">
+  <nav class="ly_inner" >
+    <ul class="icon_list">
       <li>
-        <router-link to="/" class="my_page_1">
+        <router-link to="/" class="icon_box my_page_1">
           <strong class="icon_name" >월별명세서</strong>
         </router-link>
       </li>
       <li>
-        <router-link to="/" class="my_page_2">
-          <strong class="">이용내역</strong>
+        <router-link to="/" class="icon_box my_page_2">
+          <strong class="icon_name">이용내역</strong>
         </router-link>
       </li>
       <li>
-        <router-link to="/" class="my_page_3">
-          <strong class="">다음달결제예정</strong>
+        <router-link to="/" class="icon_box my_page_3">
+          <strong class="icon_name">다음달결제예정</strong>
         </router-link>
       </li>
       <li>
-        <router-link to="/" class="my_page_4">
-          <strong class="">내가받은혜택</strong>
+        <router-link to="/" class="icon_box my_page_4">
+          <strong class="icon_name">내가받은혜택</strong>
         </router-link>
       </li>
       <li>
-        <router-link to="/" class="my_page_5">
-          <strong class="">이용한도</strong>
+        <router-link to="/" class="icon_box my_page_5">
+          <strong class="icon_name">이용한도</strong>
         </router-link>
       </li>
       <li>
-        <router-link to="/" class="my_page_6">
-          <strong class="">이용금액결제</strong>
+        <router-link to="/" class="icon_box my_page_6">
+          <strong class="icon_name">이용금액결제</strong>
         </router-link>
       </li>
       <li>
-        <router-link to="/" class="my_page_7">
-          <strong class="">전자민원접수</strong>
+        <router-link to="/" class="icon_box my_page_7">
+          <strong class="icon_name">전자민원접수</strong>
         </router-link>
       </li>
       <li>
-        <router-link to="/" class="my_page_8">
-          <strong class="">카드신청</strong>
+        <router-link to="/" class="icon_box my_page_8">
+          <strong class="icon_name">카드신청</strong>
         </router-link>
       </li>
     </ul>
