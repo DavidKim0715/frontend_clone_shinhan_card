c8e6b3fd877cd98cfba00bec6ac75a7dc43ee93c
DaivdKim0715 <sunkit0526@gmail.com>
2021-12-08 17:30:07 +0900

    ADD - 탑 헤더 메뉴 및 라우팅 추가

---
+++ src/components/common/bar/HeaderUserMenu.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav>
+    <ul class="nav-box">
+      <li v-for="(item) in items" :key="item.idx" >
+        <router-link :to="item.url">
+          {{item.name}}
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import {defineComponent, toRefs} from "vue";
+const HeaderUserMenu = defineComponent({
+  name: "HeaderUserMenu",
+  props: {
+    items : {
+      type : Array
+    }
+  },
+  setup(props, context){
+    let {items} = toRefs(props)
+    return items
+  }
+});
+export default HeaderUserMenu;
+</script>
